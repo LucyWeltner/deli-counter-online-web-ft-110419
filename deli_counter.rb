a99db506f6db05bce0c04adc5538850c24d9dde5
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-06 23:04:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(array)
   if array.size > 0 
     array.each_with_index do |name, index|
       line = line + "#{index + 1}. #{name} "
+    end
   else
     puts "The line is currently empty."
   end
