2c97f01130ec8bfb0e49319bc132545b0d6c7ad1
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-06 22:56:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,4 +2,12 @@ katz_deli = []
 def line(array)
   if array.size > 0 
     puts array 
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, person)
+  array << person
+  puts person + ", " + array.length 
 # Write your code here.
